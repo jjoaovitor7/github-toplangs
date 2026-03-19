422b196a7cc74e5ce4efa6d20d608b0e2e2ce948
João Vítor <jjoaovitor7@outlook.com.br>
2026-03-18 21:03:02 -0300

    feat: newrelic implementation.

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,8 @@ import (
   "os"
 )
 
+import "github.com/newrelic/go-agent/v3/newrelic"
+
 var PORT = os.Getenv("PORT")
 
 func main() {
@@ -17,8 +19,18 @@ func main() {
   app.SetTemplatesDir()
 
   mux := http.NewServeMux()
-  mux.HandleFunc("/", app.IndexRouteHandler)
-  mux.HandleFunc("/toplangs", app.TopLangsRouteHandler)
+//  mux.HandleFunc("/", app.IndexRouteHandler)
+//  mux.HandleFunc("/toplangs", app.TopLangsRouteHandler)
+
+  nr, _ := newrelic.NewApplication(
+    newrelic.ConfigAppName(os.Getenv("NEWRELIC_APPNAME")),
+    newrelic.ConfigLicense(os.Getenv("NEWRELIC_LICENSEKEY")),
+    newrelic.ConfigAppLogForwardingEnabled(true),
+  )
+
+  mux.HandleFunc(newrelic.WrapHandleFunc(nr, "/", app.IndexRouteHandler))
+  mux.HandleFunc(newrelic.WrapHandleFunc(nr, "/toplangs", app.TopLangsRouteHandler))
+
   logging := app.LoggingMiddleware(mux)
   log.Printf("Server started at %s", PORT)
   if err := http.ListenAndServe(PORT, logging); err != nil {
